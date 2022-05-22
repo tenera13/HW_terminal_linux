7a8d5cfdc8095988b1da5b7828378f2c712fcf12
tenera13 <alena-gvoxdeva@yandex.ru>
2022-05-23 01:06:17 +0300

    add hw

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir qa_test
+cd qa_test
+mkdir qa1
+mkdir qa2
+mkdir qa3
+cd qa1
+touch t1.txt
+touch t2.txt
+touch t3.txt
+touch j1.json
+touch j2.json
+mkdir qa01
+mkdir qa02
+mkdir qa03
+ls -la
+mv t1.txt t2.txt qa01
